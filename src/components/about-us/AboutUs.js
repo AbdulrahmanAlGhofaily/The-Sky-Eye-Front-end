124c20d7af87d7c0c645ee64592e09fdf40041e1
Abdulrahman <abdulrahman-alghofaily@outlook.com>
2023-11-30 10:29:09 +0300

    Update About us page

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -8,14 +8,14 @@ import AboutUsCard from "./AboutUsCard";
 const info = [
   {
     name: "Abdulrahman Alghofaily",
-    description: "Computer Science Graduate | Web developer | Data Scientist.",
+    description: "Fresh Computer Science graduate | Web developer | Data Scientist.",
     imageUrl: "/assets/images/abdulrahman_r.png",
     linkedin: "https://www.linkedin.com/in/abdulrahman-alghofaily/",
     github: "https://github.com/AbdulrahmanAlGhofaily",
   },
   {
     name: "Marawn Alghamdi",
-    description: "An ambitious computer science graduate | Data scientist",
+    description: "An ambitious Computer Science graduate | Data scientist",
     imageUrl: "/assets/images/marwan.JPG",
     linkedin: "https://www.linkedin.com/in/marwan-ghamdi/",
     github: "https://github.com/marwanAlghamdi/",
@@ -36,10 +36,10 @@ const info = [
   },
   {
     name: "Nawaf Alshaikh",
-    description: "Computer Science Graduate.",
+    description: "Computer Science Graduate | Interest in Data Science and ML Engineering",
     imageUrl: "/assets/images/abdulrahman_r.png",
-    linkedin: "https://www.linkedin.com/in/abdulrahman-alghofaily/",
-    github: "https://github.com/AbdulrahmanAlGhofaily",
+    linkedin: "https://www.linkedin.com/in/nawaf-alshaikh-7a35b11a2",
+    github: "https://github.com/nawaf7800/projects",
   },
 ];
 
@@ -80,11 +80,9 @@ const AboutUs = () => {
       <div className={`${classes.aboutUs} row`}>
         <div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas delectus laborum nemo dolorem sapiente
-            voluptate, quos ullam unde quod dolore quia reiciendis adipisci veniam expedita neque. Sit possimus eaque
-            nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam necessitatibus asperiores quidem iste!
-            Ea ratione eveniet animi at et similique in, officia consequatur dolorum dolores optio necessitatibus ipsam
-            explicabo neque?
+            From the heart of Riyadh, our team of SDAIA T5 bootcamp's finest computer science graduates has crafted The
+            Sky Eye AI. This pioneering tool leverages satellite imagery for detailed classification of urban
+            development, marking a new milestone in Saudi Arabia's AI-driven future.
           </p>
         </div>
         <h2 style={{ alignSelf: "center" }}>Team Members</h2>
